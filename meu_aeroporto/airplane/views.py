74be866dd29f0dfa518e88248bf4649307688acf
Gustavo Vieira <127641510+vGustav1@users.noreply.github.com>
2024-10-18 20:24:02 -0300

    Removendo comentários nos códigos

--- meu_aeroporto/airplane/views.py
+++ meu_aeroporto/airplane/views.py
@@ -1,31 +1,31 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Airplane  # Certifique-se de importar o modelo correto
+from .models import Airplane  
 
 def airplane(request):
-    myairplane = Airplane.objects.all()  # Obtendo todos os aviões
-    template = loader.get_template('all_airplane.html')  # Carregando o template de todos os aviões
+    myairplane = Airplane.objects.all() 
+    template = loader.get_template('all_airplane.html') 
     context = {
-        'myairplane': myairplane,  # Passando os aviões para o template
+        'myairplane': myairplane, 
     }
-    return HttpResponse(template.render(context, request))  # Renderizando a resposta
+    return HttpResponse(template.render(context, request))  
 
 def details(request, id):
-    myairplane = Airplane.objects.get(id=id)  # Obtendo o avião pelo ID
-    template = loader.get_template('details.html')  # Carregando o template de detalhes
+    myairplane = Airplane.objects.get(id=id)  
+    template = loader.get_template('details.html')  
     context = {
-        'myairplane': myairplane,  # Passando o avião para o template
+        'myairplane': myairplane,  
     }
-    return HttpResponse(template.render(context, request))  # Renderizando a resposta
+    return HttpResponse(template.render(context, request))  
 
 def main(request):
-    template = loader.get_template('main.html')  # Carregando o template principal
-    return HttpResponse(template.render())  # Renderizando a resposta
+    template = loader.get_template('main.html')  
+    return HttpResponse(template.render())  
 
 def testing(request):
-    mydata = Airplane.objects.all().order_by('modelo')  # Obtendo todos os aviões ordenados pelo modelo
-    template = loader.get_template('template.html')  # Carregando o template de teste
+    mydata = Airplane.objects.all().order_by('modelo')  
+    template = loader.get_template('template.html')  
     context = {
-        'myairplane': mydata,  # Passando os aviões para o template
+        'myairplane': mydata,  
     }
-    return HttpResponse(template.render(context, request))  # Renderizando a resposta
+    return HttpResponse(template.render(context, request))  
